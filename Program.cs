8a0c6defa54832c944f2e450bc35d6d883ddf82e
Aliya Mullina <aliya.mullina@ya.ru>
2021-02-05 15:59:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_for_YandexDiskAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
